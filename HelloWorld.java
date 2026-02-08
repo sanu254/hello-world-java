e4715475210b64e0cef333d9018a97d40d9d1e97
sanu254 <saniyabety254@gmail.com>
2026-02-08 17:16:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
    public class HelloWorld{
-         pubic static void main(String[] args) {
+         public static void main(String[] args) {
               System.out.println("Hello Jenkins");
     }
        }
+
 
 
 
